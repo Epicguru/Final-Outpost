8b75b6fc072b59e3944974d2570daa4662963cea
James Billy <james@epicguru.co.uk>
2017-02-20 19:49:33 +0100

    Added FinalOutpost plugin project.

---
+++ FinalOutpost/src/main/java/co/uk/epicguru/finaloutpost/main/Main.java
@@ -0,0 +1,13 @@
+package co.uk.epicguru.finaloutpost.main;
+
+import ro.fortsoft.pf4j.Plugin;
+import ro.fortsoft.pf4j.PluginWrapper;
+
+public class Main extends Plugin{
+
+	public Main(PluginWrapper wrapper) {
+		super(wrapper);
+		// SAMPLE
+	}
+
+}
