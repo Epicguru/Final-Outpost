e62f502c195b009184eac3c1df2c8936f9f0287a
James Billy <james@epicguru.co.uk>
2017-02-22 20:00:08 +0100

    Added placeholders and gradle tasks.
    Order of tasks is :plugin, copyPlugins, :run

--- FinalOutpost/src/main/java/co/uk/epicguru/finaloutpost/main/Main.java
+++ FinalOutpost/src/main/java/co/uk/epicguru/finaloutpost/main/Main.java
@@ -1,13 +1,27 @@
 package co.uk.epicguru.finaloutpost.main;
 
-import ro.fortsoft.pf4j.Plugin;
+import co.uk.epicguru.API.FinalOutpostPlugin;
+import co.uk.epicguru.logging.Log;
+import ro.fortsoft.pf4j.PluginException;
 import ro.fortsoft.pf4j.PluginWrapper;
 
-public class Main extends Plugin{
+public class Main extends FinalOutpostPlugin{
 
+	public static final String version = "0.0.0";
+	public static final String TAG = "Final Outpost Plugin";	
+	
 	public Main(PluginWrapper wrapper) {
-		super(wrapper);
-		// SAMPLE
+		super(wrapper, "Final Outpost Core", version);
+	}
+
+	@Override
+	public void start() throws PluginException {
+		Log.info(TAG, "Started plugin");
+	}
+
+	@Override
+	public void stop() throws PluginException {
+		Log.info(TAG, "Stopping plugin");
 	}
 
 }
