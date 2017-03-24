379aa71f491843dccf8ce2a4b3aa986e5eb3aa94
James Billy <james@epicguru.co.uk>
2017-03-24 18:20:24 +0100

    Documented JPhysics

--- Engine/src/main/java/co/uk/epicguru/physics/JPhysics.java
+++ Engine/src/main/java/co/uk/epicguru/physics/JPhysics.java
@@ -16,10 +16,22 @@ public class JPhysics {
 		Log.info("JPhysics", object == null ? "Null" : object.toString()); // Local
 	}
 	
+	/**
+	 * Sets the amount of pixels per in-game meter. A good size is 32.
+	 * Generally there is no performance advantage of having any size. However rendering objects with a meter system is often simpler.
+	 * Never set this while a physics simulation is running, as it can ruin scenes and create unstable or broken physics.
+	 * @param PPM The pixels that are in each meter.
+	 * @see {@link #getPPM()}.
+	 */
 	public void setPPM(float PPM){
 		this.PPM = PPM;
 	}
 	
+	/**
+	 * Gets the amount of pixels in a in-game meter, as set in {@link #setPPM(float)}.
+	 * The default value of this is 1.
+	 * @return The pixels in one meter.
+	 */
 	public float getPPM(){
 		return PPM;
 	}
