4d9f25315e4a97ea5f31a5fe1a98dd1b14dfde47
James Billy <james@epicguru.co.uk>
2017-04-16 15:45:50 +0200

    Engine documentation

--- Engine/src/main/java/co/uk/epicguru/entity/engine/Engine.java
+++ Engine/src/main/java/co/uk/epicguru/entity/engine/Engine.java
@@ -26,14 +26,26 @@ public class Engine implements Disposable{
 	
 	private ArrayList<Entity> group = new ArrayList<Entity>();
 	
+	/**
+	 * Gets the Box2D world.
+	 */
 	public World getWorld(){
 		return this.world;
 	}
 	
+	/**
+	 * Sets the Box2D world. Please do not use this!
+	 */
 	public World setWorld(World world){
 		return this.world = world;
 	}
 	
+	/**
+	 * Gets all entities of a particular {@link Group}. This ALWAYS returns the same array list so please
+	 * do not keep the array, just read it and leave it! (I'm a poet and I know it).
+	 * @param group The Group to get entities for.
+	 * @return An ArrayList of entities that match the criteria.
+	 */
 	public ArrayList<Entity> ofGroup(Group group){
 		if(group == null){
 			return null;
@@ -49,28 +61,49 @@ public class Engine implements Disposable{
 		return this.group;
 	}
 	
+	/**
+	 * Gets all entities of a particular {@link Group}. This creates a new array and so is safe to store unlike {@link #ofGroup(Group)}.
+	 * @param group The Group to get entities for.
+	 * @return An array of entities that match the criteria.
+	 */
 	public Entity[] ofGroupArray(Group group){
 		return this.ofGroupArray(group, new Entity[0]);
 	}
 	
+	/**
+	 * Gets all entities of a particular {@link Group}. The entities are packed into the array specified and so is safe to store unlike {@link #ofGroup(Group)}.
+	 * @param group The Group to get entities for.
+	 * @return An array of entities that match the criteria.
+	 */
 	public Entity[] ofGroupArray(Group group, Entity[] array){
 		ArrayList<Entity> e = this.ofGroup(group);
 		return e == null ? null : e.toArray(array);
 	}
 	
+	/**
+	 * Removes all entities from the world.
+	 */
 	public void clearEntities(){
 		this.addNew();
 		for(Entity e : entities){
 			bin.add(e);
 		}
-		flush();	
+		this.flush();	
 	}
-	
+
+	/**
+	 * Adds an entity to this Engine.
+	 * @param e The entity to add, if null ignored.
+	 */
 	public void add(Entity e){
 		if(e != null)
 			this.add.add(e);
 	}
 	
+	/**
+	 * Removes an entity from the Engine.
+	 * @param e The entity to add, if null ignored.
+	 */
 	public void remove(Entity e){
 		if(e != null)
 			this.bin.add(e);
@@ -92,6 +125,10 @@ public class Engine implements Disposable{
 		bin.clear();
 	}
 
+	/**
+	 * Updates the Box2D world that this Engine manages. See {@link #getWorld()}.
+	 * @param delta The delta-time value.
+	 */
 	public void updateWorld(float delta){
 		if(this.getWorld() == null)
 			return;
@@ -99,10 +136,17 @@ public class Engine implements Disposable{
 		this.getWorld().step(delta, 8, 3);
 	}
 	
+	/**
+	 * Gets all active entities. DO NOT MANIPULATE ARRAY!!!!
+	 */
 	public ArrayList<Entity> getAllEntities(){
 		return entities;
 	}
 	
+	/**
+	 * Updates the entities in this engine.
+	 * @param delta The delta-time value.
+	 */
 	public void update(float delta){
 		this.addNew();
 		this.flush();
@@ -114,6 +158,11 @@ public class Engine implements Disposable{
 		this.flush();
 	}
 	
+	/**
+	 * Updates the entities in this engine.
+	 * @param batch The batch to render entities with.
+	 * @param delta The delta-time value.
+	 */
 	public void render(Batch batch, float delta){
 		for(Entity e : entities){
 			e.render(batch, delta);
@@ -122,6 +171,10 @@ public class Engine implements Disposable{
 		this.flush();
 	}
 	
+	/**
+	 * Renders UI elements of the entities in this engine.
+	 * @param delta The delta-time value.
+	 */
 	public void renderUI(Batch batch, float delta){
 		for(Entity e : entities){
 			e.renderUI(batch, delta);
@@ -130,6 +183,9 @@ public class Engine implements Disposable{
 		this.flush();
 	}
 	
+	/**
+	 * Internal.
+	 */
 	public void flushBodies(){
 		
 		if(this.getWorld().isLocked() || this.getWorld() == null)
@@ -149,12 +205,18 @@ public class Engine implements Disposable{
 		bodyBin.clear();
 	}
 	
+	/**
+	 * Creates a new BodyDef where the linear dampening is set to 0.5 .
+	 */
 	public BodyDef newBodyDef(){
 		BodyDef def = new BodyDef();
 		def.linearDamping = 0.5f;
 		return def;
 	}
 	
+	/**
+	 * Creates a new PolygonShape where the origin is (0, 0) and the width and height are measured in tiles.
+	 */
 	public PolygonShape boxOfSize(float width, float height){
 		
 		// Make a polygon shape where the origin will be (0, 0) on the box. Good for rendering.
@@ -166,6 +228,12 @@ public class Engine implements Disposable{
 		return shape;
 	}
 	
+	/**
+	 * Creates a new Box2D body and adds it to the physics world.
+	 * @param e The Entity to associate the body to.
+	 * @param def The BodyDef to base the body off.
+	 * @return The new Body.
+	 */
 	public Body newBody(Entity e, BodyDef def){
 		if(this.getWorld() == null)
 			return null;
@@ -175,6 +243,9 @@ public class Engine implements Disposable{
 		return b;
 	}
 	
+	/**
+	 * Disposes the Box2D world.
+	 */
 	public void disposeWorld(){
 		if(this.getWorld() == null)
 			return;
@@ -184,10 +255,19 @@ public class Engine implements Disposable{
 		this.setWorld(null);
 	}
 	
+	/**
+	 * Safely removes a Box2D body from the world.
+	 * @param body The body to remove.
+	 * @param run The optional runnable object that will be called once the body is removed, as it may take a frame or two to remove
+	 * it properly.
+	 */
 	public void removeBody(Body body, Runnable run){
 		this.bodyBin.add(new DeadBody(body, run));
 	}
 	
+	/**
+	 * Disposes the whole Engine.
+	 */
 	public void dispose(){
 		this.clearEntities();
 		this.flushBodies();
