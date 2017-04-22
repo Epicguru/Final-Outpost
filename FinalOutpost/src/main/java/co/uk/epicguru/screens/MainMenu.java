369776b70411be02848e1f5e5afb411784757031
James Billy <james@epicguru.co.uk>
2017-04-22 20:07:56 +0200

    Animated main menu

--- FinalOutpost/src/main/java/co/uk/epicguru/screens/MainMenu.java
+++ FinalOutpost/src/main/java/co/uk/epicguru/screens/MainMenu.java
@@ -1,9 +1,12 @@
 package co.uk.epicguru.screens;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Interpolation;
+import com.badlogic.gdx.math.MathUtils;
 
 import co.uk.epicguru.API.screens.GameScreen;
 import co.uk.epicguru.UI.Image;
@@ -19,6 +22,9 @@ public final class MainMenu extends GameScreen {
 	public TextureRegion background;
 	public TextureRegion title;
 	public Observer obs;
+	private float p = 0;
+	private float direction = 1;
+	private float timer = 0;
 	
 	public Image titleImage;
 	
@@ -33,6 +39,12 @@ public final class MainMenu extends GameScreen {
 		
 		// UI elements
 		this.titleImage = new Image(this.title);
+		
+		// Interpolation effects
+		p = 0;
+		
+		// Misc
+		timer = 0;
 		
 		// Hooks
 		super.show();
@@ -52,6 +64,9 @@ public final class MainMenu extends GameScreen {
 	
 	public void update(float delta){
 		
+		// Timer
+		timer += delta;
+		
 		// Made for a 1080p monitor : we need to find the right scale for other monitor resolutions. We will simply scale
 		// up or down depending on the resolution aspect, using horizontal pixels.
 		// (Will this affect ultra and super wide monitor users? Ooops.)
@@ -60,8 +75,22 @@ public final class MainMenu extends GameScreen {
 		float scale = Gdx.graphics.getWidth() / 1920f;
 		obs.setScale(scale); // Something for 4k ?? :D
 		
-		// Title
-		titleImage.bounds.setPosition(0, Gdx.graphics.getHeight() - titleImage.getFinalHeight(obs) + titleImage.getFinalHeight(obs) / 5f);
+		// Interpolation effects percentage
+		float seconds = 2; // Lasts 2 seconds
+		p += (delta * direction) / seconds;
+		if(p > 1)
+			p = 1;
+		if(p < 0)
+			p = 0;
+		
+		if(Input.isKeyJustDown(Keys.Z)){
+			this.direction = 1;
+			this.p = 0;
+		}
+		if(Input.isKeyJustDown(Keys.X)){
+			this.direction = -1;
+			this.p = 1;
+		}
 		
 		// Hooks
 		super.update(delta);
@@ -71,6 +100,26 @@ public final class MainMenu extends GameScreen {
 		
 		// Hooks
 		super.render(delta, batch);
+	}
+	
+	public void cogs(float delta, Batch batch){
+		Interpolation i = Interpolation.swing;
+		
+		float add = 200;
+		
+		LoadingSymbol.render(i.apply(-add, 0, this.p), 0, obs, delta / 2f);
+		LoadingSymbol.render(i.apply(Gdx.graphics.getWidth() + add, Gdx.graphics.getWidth(), this.p), 0, obs, delta / 2f);
+	}
+	
+	public void title(float delta, Batch batch){
+		Interpolation i = Interpolation.pow4;
+		
+		// Position
+		float x = i.apply(Gdx.graphics.getWidth(), 0, this.p);
+		titleImage.bounds.setPosition(x,
+				Gdx.graphics.getHeight() - titleImage.getFinalHeight(obs) + titleImage.getFinalHeight(obs) / 5f + MathUtils.cosDeg(timer * 80f) * 15f);
+	
+		titleImage.render(obs, delta);
 	}
 	
 	public void renderUI(float delta, Batch batch){	
@@ -84,19 +133,19 @@ public final class MainMenu extends GameScreen {
 		float addX = Input.getMouseX() - hw;
 		float addY = getScreenHeight() + Input.getMouseY() - hh;
 		
-		addX *= 0.1f;
-		addY *= 0.1f;
+		addX *= 0.05f;
+		addY *= 0.05f;
 		
 		// Background
-		super.renderToSize(batch, this.background, hw + addX, hh + addY, getScreenWidth() * m, getScreenHeight() * m);		
+		batch.setColor(new Color(p, p, p, 1));
+		super.renderToSize(batch, this.background, hw + addX, hh + addY, getScreenWidth() * m, getScreenHeight() * m);	
+		batch.setColor(Color.WHITE);
 		
 		// Title
-		titleImage.render(obs, delta);
+		this.title(delta, batch);
 		
-		// Just cuz
-		LoadingSymbol.render(0, 0, obs, delta / 3f);
-		LoadingSymbol.render(Gdx.graphics.getWidth(), 0, obs, delta / 3f);
-		LoadingSymbol.render(Input.getMouseX(), Input.getMouseY(), obs, delta / 3f);
+		// Nice!
+		this.cogs(delta, batch);
 		
 		// Hooks
 		super.renderUI(delta, batch);
