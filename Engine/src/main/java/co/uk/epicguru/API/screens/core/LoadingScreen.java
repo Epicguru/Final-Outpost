66c50a73bcc8e0588e5168a0ab23a05b2eaff6fa
James Billy <james@epicguru.co.uk>
2017-07-22 22:05:38 +0200

    Stupid loading screen change :D

--- Engine/src/main/java/co/uk/epicguru/API/screens/core/LoadingScreen.java
+++ Engine/src/main/java/co/uk/epicguru/API/screens/core/LoadingScreen.java
@@ -11,6 +11,10 @@ import net.lingala.zip4j.core.ZipFile;
 public class LoadingScreen extends GameScreen {
 
 	public BitmapFont font = new BitmapFont();
+	private float timer;
+	private int index;
+	private final int size = 100;
+	private StringBuilder str = new StringBuilder();
 
 	public void update(float delta){
 		if(FOE.loaded)
@@ -24,11 +28,57 @@ public class LoadingScreen extends GameScreen {
 						+ U.nameFromPath(file.getProgressMonitor().getFileName());
 			}
 		}
+		
+		this.updateTimer(delta);
 	}
 
+	private void updateTimer(float delta){
+		
+		timer += delta;
+		float interval = 0.01f;
+		while(timer >= interval){
+			timer -= interval;
+			
+			index++;
+			if(index >= size){
+				index = 0;
+			}
+			
+			this.updateStringBuidler();
+		}
+		
+	}
+	
+	private void updateStringBuidler(){
+		
+		str.setLength(0);
+		
+		// -------<||||||||>------
+		
+		for(int i = 0; i < size; i++){
+			int dst = Math.abs(index - i);
+			
+			if(i == 0){
+				str.append('#');
+			}else if(i == size - 1){
+				str.append('#');
+			}else if(dst <= 5){
+				str.append('|');
+			}else if(index - i == 6){
+				str.append('<');
+			}else if(index - i == -6){
+				str.append('>');
+			}else{
+				str.append('-');
+			}
+		}
+		
+	}
+	
 	public void renderUI(float delta, Batch batch){
 		font.draw(batch, FOE.loadingText == null ? "" : FOE.loadingText, getScreenWidth() / 2f, getScreenHeight() / 2f, -1, 1, false);
 		font.draw(batch, FOE.loadingSubText == null ? "" : FOE.loadingSubText, getScreenWidth() / 2f, getScreenHeight() / 2f - 20, -1, 1, false);
+		font.draw(batch, this.str.toString(), getScreenWidth() / 2f, getScreenHeight() / 2f - 40, -1, 1, false);
 	}
 
 }
